993869779ddad82c2f131f13d51a7c546899ec52
sife <sa_9_8@yahoo.com>
2025-02-27 07:41:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def send_azkar():
     while True:
         azkar = random.choice(AZKAR_LIST)
         ramadan_msg = get_ramadan_message()
-        message = f"{azkar}\n\n{ramadan_msg}\n\nبوت🌙 sa forex اذكار رمضان 🌙".encode('utf-16', 'surrogatepass').decode('utf-16')
+        message = f"{azkar}\n\n{ramadan_msg}\n\n🌙 بوت sa forex اذكار رمضان 🌙".encode('utf-16', 'surrogatepass').decode('utf-16')
         try:
             await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode=telegram.constants.ParseMode.HTML)
         except Exception as e:
